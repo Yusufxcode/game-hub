2726782fd4fd77dfd1b1b53a6ec9f068f6e27914
Adebakin Yusuf <109008836+Yusufxcode@users.noreply.github.com>
2023-06-28 08:04:45 +0100

    Refactoring- with React Query

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,7 @@
+import { useQuery } from "@tanstack/react-query";
 import { GameQuery } from "../App";
-import useData from "./useData";
+import apiClient from "../service/api-client";
+import { FetchResponse } from "../service/api-client";
 
 export interface Platform {
     id: number
@@ -10,19 +12,27 @@ export interface Platform {
 export interface Game {
     id: number;
     name: string;
-    background_image: string
+    background_image: string;
     parent_platforms: { platform: Platform}[]
     metacritic: number
   }
   
 const useGames = (gameQuery: GameQuery) => 
-useData<Game>('/games', 
-{params: {
-  genres: gameQuery.genre?.id, 
-  platforms: gameQuery.platform?.id,
-  ordering: gameQuery.sortOrder,
-  search: gameQuery.searchText
-}}, 
-[gameQuery])
+useQuery<FetchResponse<Game>, Error>({
+  queryKey: ['games', gameQuery],
+  queryFn: () =>
+    apiClient
+      .get<FetchResponse<Game>>('/games', {
+        params: {
+          genres: gameQuery.genre?.id, 
+          parent_platforms: gameQuery.platform?.id,
+          ordering: gameQuery.sortOrder,
+          search: gameQuery.searchText
+        }
+      })
+      .then(res => res.data)
+
+})
+
 
 export default useGames;
