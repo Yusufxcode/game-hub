e802bc7dcafb1bb79dde6e96e70e9257eddcd3b0
Adebakin Yusuf <109008836+Yusufxcode@users.noreply.github.com>
2023-07-17 15:41:32 +0100

    Build game screenshots

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,12 +1,10 @@
+import { Heading, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
-import useGame from "../hooks/useGame";
-import { Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
-import GameTrailer from "../components/GameTrailer";
-import DefinitionItem from "../components/DefinitionItem";
-import CriticScore from "../components/CriticScore";
-import genres from "../data/genres";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
+import useGame from "../hooks/useGame";
+import Screenshots from "../components/Screenshots";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
@@ -21,6 +19,7 @@ const GameDetailPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <Screenshots gameId={game.id} />
       <GameTrailer gameId={game.id} />
     </>
   );
